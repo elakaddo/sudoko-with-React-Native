6d0d4fa740ae20cc6544340fa081646420348a5d
Samir <elakaddo@gmail.com>
2022-11-03 14:36:32 +0100

    first commit

---
+++ App.js
@@ -0,0 +1,239 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+         {/* Grid */}
+        <View style={styles.grid}>
+
+                {/* Line 1 */}
+                <View style={styles.line}>
+
+                {/* Square 1 */}
+                <View style={styles.square}>
+                        <View style={styles.lineSquare}>
+                              <View style={styles.cell}> <Text style={styles.number}>1</Text> </View>
+                              <View style={styles.cell}> <Text style={styles.number}>1</Text> </View>
+                              <View style={styles.cell}> <Text style={styles.number}>1</Text> </View>
+                        </View>
+                        <View style={styles.lineSquare}>
+                              <View style={styles.cell}> <Text style={styles.number}>1</Text> </View>
+                              <View style={styles.cell}> <Text style={styles.number}>1</Text> </View>
+                              <View style={styles.cell}> <Text style={styles.number}>1</Text> </View>
+                        </View>
+                        <View style={styles.lineSquare}>
+                              <View style={styles.cell}> <Text style={styles.number}>1</Text> </View>
+                              <View style={styles.cell}> <Text style={styles.number}>1</Text> </View>
+                              <View style={styles.cell}> <Text style={styles.number}>1</Text> </View>
+                        </View>
+                </View>
+
+                {/* Square 2 */}
+                <View style={styles.square}>
+                        <View style={styles.lineSquare}>
+                              <View style={styles.cell}> <Text style={styles.number}>1</Text> </View>
+                              <View style={styles.cell}> <Text style={styles.number}>1</Text> </View>
+                              <View style={styles.cell}> <Text style={styles.number}>1</Text> </View>
+                        </View>
+                        <View style={styles.lineSquare}>
+                              <View style={styles.cell}> <Text style={styles.number}>1</Text> </View>
+                              <View style={styles.cell}> <Text style={styles.number}>1</Text> </View>
+                              <View style={styles.cell}> <Text style={styles.number}>1</Text> </View>
+                        </View>
+                        <View style={styles.lineSquare}>
+                              <View style={styles.cell}> <Text style={styles.number}>1</Text> </View>
+                              <View style={styles.cell}> <Text style={styles.number}>1</Text> </View>
+                              <View style={styles.cell}> <Text style={styles.number}>1</Text> </View>
+                        </View>
+                </View>
+
+                {/* Square 3 */}
+                <View style={styles.square}>
+                        <View style={styles.lineSquare}>
+                              <View style={styles.cell}> <Text style={styles.number}>1</Text> </View>
+                              <View style={styles.cell}> <Text style={styles.number}>1</Text> </View>
+                              <View style={styles.cell}> <Text style={styles.number}>1</Text> </View>
+                        </View>
+                        <View style={styles.lineSquare}>
+                              <View style={styles.cell}> <Text style={styles.number}>1</Text> </View>
+                              <View style={styles.cell}> <Text style={styles.number}>1</Text> </View>
+                              <View style={styles.cell}> <Text style={styles.number}>1</Text> </View>
+                        </View>
+                        <View style={styles.lineSquare}>
+                              <View style={styles.cell}> <Text style={styles.number}>1</Text> </View>
+                              <View style={styles.cell}> <Text style={styles.number}>1</Text> </View>
+                              <View style={styles.cell}> <Text style={styles.number}>1</Text> </View>
+                        </View>
+                </View>
+                </View>
+
+                 {/* Line 2 */}
+              <View style={styles.line}>
+
+                    {/* Square 1 */}
+                    <View style={styles.square}>
+                            <View style={styles.lineSquare}>
+                                  <View style={styles.cell}> <Text style={styles.number}>1</Text> </View>
+                                  <View style={styles.cell}> <Text style={styles.number}>1</Text> </View>
+                                  <View style={styles.cell}> <Text style={styles.number}>1</Text> </View>
+                            </View>
+                            <View style={styles.lineSquare}>
+                                  <View style={styles.cell}> <Text style={styles.number}>1</Text> </View>
+                                  <View style={styles.cell}> <Text style={styles.number}>1</Text> </View>
+                                  <View style={styles.cell}> <Text style={styles.number}>1</Text> </View>
+                            </View>
+                            <View style={styles.lineSquare}>
+                                  <View style={styles.cell}> <Text style={styles.number}>1</Text> </View>
+                                  <View style={styles.cell}> <Text style={styles.number}>1</Text> </View>
+                                  <View style={styles.cell}> <Text style={styles.number}>1</Text> </View>
+                            </View>
+                    </View>
+
+                    {/* Square 2 */}
+                    <View style={styles.square}>
+                            <View style={styles.lineSquare}>
+                                  <View style={styles.cell}> <Text style={styles.number}>1</Text> </View>
+                                  <View style={styles.cell}> <Text style={styles.number}>1</Text> </View>
+                                  <View style={styles.cell}> <Text style={styles.number}>1</Text> </View>
+                            </View>
+                            <View style={styles.lineSquare}>
+                                  <View style={styles.cell}> <Text style={styles.number}>1</Text> </View>
+                                  <View style={styles.cell}> <Text style={styles.number}>1</Text> </View>
+                                  <View style={styles.cell}> <Text style={styles.number}>1</Text> </View>
+                            </View>
+                            <View style={styles.lineSquare}>
+                                  <View style={styles.cell}> <Text style={styles.number}>1</Text> </View>
+                                  <View style={styles.cell}> <Text style={styles.number}>1</Text> </View>
+                                  <View style={styles.cell}> <Text style={styles.number}>1</Text> </View>
+                            </View>
+                    </View>
+
+                    {/* Square 3 */}
+                    <View style={styles.square}>
+                            <View style={styles.lineSquare}>
+                                  <View style={styles.cell}> <Text style={styles.number}>1</Text> </View>
+                                  <View style={styles.cell}> <Text style={styles.number}>1</Text> </View>
+                                  <View style={styles.cell}> <Text style={styles.number}>1</Text> </View>
+                            </View>
+                            <View style={styles.lineSquare}>
+                                  <View style={styles.cell}> <Text style={styles.number}>1</Text> </View>
+                                  <View style={styles.cell}> <Text style={styles.number}>1</Text> </View>
+                                  <View style={styles.cell}> <Text style={styles.number}>1</Text> </View>
+                            </View>
+                            <View style={styles.lineSquare}>
+                                  <View style={styles.cell}> <Text style={styles.number}>1</Text> </View>
+                                  <View style={styles.cell}> <Text style={styles.number}>1</Text> </View>
+                                  <View style={styles.cell}> <Text style={styles.number}>1</Text> </View>
+                            </View>
+                    </View>
+              </View>
+
+              {/* Line 3 */}
+              <View style={styles.line}>
+                
+                        {/* Square 1 */}
+                        <View style={styles.square}>
+                                <View style={styles.lineSquare}>
+                                      <View style={styles.cell}> <Text style={styles.number}>1</Text> </View>
+                                      <View style={styles.cell}> <Text style={styles.number}>1</Text> </View>
+                                      <View style={styles.cell}> <Text style={styles.number}>1</Text> </View>
+                                </View>
+                                <View style={styles.lineSquare}>
+                                      <View style={styles.cell}> <Text style={styles.number}>1</Text> </View>
+                                      <View style={styles.cell}> <Text style={styles.number}>1</Text> </View>
+                                      <View style={styles.cell}> <Text style={styles.number}>1</Text> </View>
+                                </View>
+                                <View style={styles.lineSquare}>
+                                      <View style={styles.cell}> <Text style={styles.number}>1</Text> </View>
+                                      <View style={styles.cell}> <Text style={styles.number}>1</Text> </View>
+                                      <View style={styles.cell}> <Text style={styles.number}>1</Text> </View>
+                                </View>
+                        </View>
+
+                        {/* Square 2 */}
+                        <View style={styles.square}>
+                                <View style={styles.lineSquare}>
+                                      <View style={styles.cell}> <Text style={styles.number}>1</Text> </View>
+                                      <View style={styles.cell}> <Text style={styles.number}>1</Text> </View>
+                                      <View style={styles.cell}> <Text style={styles.number}>1</Text> </View>
+                                </View>
+                                <View style={styles.lineSquare}>
+                                      <View style={styles.cell}> <Text style={styles.number}>1</Text> </View>
+                                      <View style={styles.cell}> <Text style={styles.number}>1</Text> </View>
+                                      <View style={styles.cell}> <Text style={styles.number}>1</Text> </View>
+                                </View>
+                                <View style={styles.lineSquare}>
+                                      <View style={styles.cell}> <Text style={styles.number}>1</Text> </View>
+                                      <View style={styles.cell}> <Text style={styles.number}>1</Text> </View>
+                                      <View style={styles.cell}> <Text style={styles.number}>1</Text> </View>
+                                </View>
+                        </View>
+
+                        {/* Square 3 */}
+                        <View style={styles.square}>
+                                <View style={styles.lineSquare}>
+                                      <View style={styles.cell}> <Text style={styles.number}>1</Text> </View>
+                                      <View style={styles.cell}> <Text style={styles.number}>1</Text> </View>
+                                      <View style={styles.cell}> <Text style={styles.number}>1</Text> </View>
+                                </View>
+                                <View style={styles.lineSquare}>
+                                      <View style={styles.cell}> <Text style={styles.number}>1</Text> </View>
+                                      <View style={styles.cell}> <Text style={styles.number}>1</Text> </View>
+                                      <View style={styles.cell}> <Text style={styles.number}>1</Text> </View>
+                                </View>
+                                <View style={styles.lineSquare}>
+                                      <View style={styles.cell}> <Text style={styles.number}>1</Text> </View>
+                                      <View style={styles.cell}> <Text style={styles.number}>1</Text> </View>
+                                      <View style={styles.cell}> <Text style={styles.number}>1</Text> </View>
+                                </View>
+                        </View>
+              </View>
+
+        </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    //alignItems: 'center',
+    justifyContent: 'center',
+  },
+  grid:{
+    flex: 1,
+    flexDirection: 'column',
+},
+line:{
+    flex: 1,
+    flexDirection:'row',
+    borderWidth: 8,
+    borderColor: 'lightgray',
+},
+square:{
+    flex: 1,
+    flexDirection: 'column',
+    borderWidth: 4,
+    borderColor: 'gray',
+    
+},
+lineSquare:{
+    flex:1,
+    flexDirection:'row',
+    borderWidth: 2,
+    borderColor: 'lightgreen',
+},
+cell:{
+    flex:1,
+    justifyContent:'center',
+    borderWidth: 1,
+    borderColor: 'green',
+},
+number:{
+  fontSize:24,
+  textAlign:'center'
+}
+});
